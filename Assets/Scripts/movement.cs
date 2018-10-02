fb1f62c72bfad838b800dfb464c45af501757074
Cindy <cl4208@nyu.edu>
2018-10-02 02:45:50 -0400

    Implemented jumping

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+	public float jumpForce = 100f;
+	public float jumpTimer = .5f;
+
+	public bool canJump;
+	public Rigidbody capPlayer;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		canJump = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+       	{
+       		transform.Translate(0f, 0f, 0.3f);
+       	}
+		
+		if (Input.GetKey(KeyCode.S))
+       	{
+       		transform.Translate(0f, 0f, -0.3f);
+       	}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(0f, 5f, 0f);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+       	{
+       		transform.Rotate(0f, -5f, 0f);
+       	}
+
+	}
+
+	void FixedUpdate()
+	{
+		if (canJump == true)
+		{
+			if (Input.GetKey(KeyCode.Space) && jumpTimer >= 0) 
+			{
+				capPlayer.AddForce(transform.up * jumpForce);
+				jumpTimer -= Time.deltaTime;
+			}
+			
+			else if (jumpTimer < 0) 
+			{
+				capPlayer.AddForce(transform.up * 0);
+			}
+		}
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == "Ground")
+		{
+			canJump = true;
+			jumpTimer = .5f;
+		}
+	}
+}
