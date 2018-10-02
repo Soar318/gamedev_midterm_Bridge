1b7a389ae1b66df7a9283d35b0707db790a1726b
Cindy <cl4208@nyu.edu>
2018-10-02 03:46:25 -0400

    Added collecting and stacking rock mechanic

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Xsl;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour
 {
-	public float jumpForce = 100f;
+	public float jumpForce = 75f;
 	public float jumpTimer = .5f;
+
+	public int rockCounter;
 
 	public bool canJump;
 	public Rigidbody capPlayer;
@@ -15,28 +19,31 @@ public class movement : MonoBehaviour
 	void Start ()
 	{
 		canJump = false;
+		jumpTimer = .5f;
+		jumpForce = 75f;
+		rockCounter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.W))
        	{
-       		transform.Translate(0f, 0f, 0.3f);
+       		transform.Translate(0f, 0f, 0.2f);
        	}
 		
 		if (Input.GetKey(KeyCode.S))
        	{
-       		transform.Translate(0f, 0f, -0.3f);
+       		transform.Translate(0f, 0f, -0.2f);
        	}
 
 		if (Input.GetKey(KeyCode.D))
 		{
-			transform.Rotate(0f, 5f, 0f);
+			transform.Rotate(0f, 3f, 0f);
 		}
 
 		if (Input.GetKey(KeyCode.A))
        	{
-       		transform.Rotate(0f, -5f, 0f);
+       		transform.Rotate(0f, -3f, 0f);
        	}
 
 	}
@@ -64,6 +71,16 @@ public class movement : MonoBehaviour
 		{
 			canJump = true;
 			jumpTimer = .5f;
+		}
+
+		if (collision.gameObject.tag == "Water")
+		{
+			Destroy(gameObject);
+		}
+
+		if (collision.gameObject.tag == "Rock")
+		{
+			rockCounter += 1;
 		}
 	}
 }
---
+++ Assets/Scripts/rockStacking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rockStacking : MonoBehaviour
+{
+
+	public GameObject player;
+	public GameObject rock;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (player.GetComponent<movement>().rockCounter == 1)
+		{
+			Instantiate(rock);
+		}
+	}
+}
