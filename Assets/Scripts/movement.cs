9db027055dab4f0280126ed49db602eab7418287
Cindy <cl4208@nyu.edu>
2018-10-11 01:52:38 -0400

    Fixed loading rocks

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -6,26 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour
 {
-	public float jumpForce = 75f;
-	public float jumpTimer = .5f;
+	public float jumpForce = 2f; //how much force is applied, I have it super high cuz high
+                               //grav feels better
+	
+	public float jumpTimer = .3f; //counter for how long the player can jump for, .5f is sorta
+                               //a glide at this point
 
-	public int rockCounter;
+	public int rockCounter; //nothing u need to worry bout
 
-	public bool canJump;
-	public Rigidbody capPlayer;
+	public bool canJump; //bool for whether or not player can jump at all, is true if it hits
+                      //the ground, is false if player is in the air
+	
+	public Rigidbody capPlayer; //rigidbody of the player object
 	
 	
 	// Use this for initialization
 	void Start ()
 	{
-		canJump = false;
-		jumpTimer = .5f;
-		jumpForce = 75f;
-		rockCounter = 0;
+		canJump = false; //my player spawns in the air, so at the start player cannot jump until
+                   //they hit the ground
+		
+		//setting some values dont worry bout it
+		jumpTimer = .3f; 
+		jumpForce = 2f;
+		rockCounter = 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		//movement code for forward, backward, and left and right rotation
 		if (Input.GetKey(KeyCode.W))
        	{
        		transform.Translate(0f, 0f, 0.2f);
@@ -50,32 +60,38 @@ public class movement : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		if (canJump == true)
+		if (canJump == true) //if player can jump...
 		{
-			if (Input.GetKey(KeyCode.Space) && jumpTimer >= 0) 
+			if (Input.GetKey(KeyCode.Space) && jumpTimer >= 0) //if player presses space AND the
+                                                      //jump timer is greater than or less than zero
 			{
-				capPlayer.AddForce(transform.up * jumpForce);
-				jumpTimer -= Time.deltaTime;
+				
+				capPlayer.AddForce(transform.up * jumpForce, ForceMode.Impulse); // apply force upward multiplied
+                                                  // by the amount of force
+				
+				jumpTimer -= Time.deltaTime; //amount of time player can jump for goes down
 			}
 			
-			else if (jumpTimer < 0) 
+			else if (jumpTimer < 0) //once the timer hits less than zero...
 			{
-				capPlayer.AddForce(transform.up * 0);
+				capPlayer.AddForce(transform.up * 0); //force is multiplied by zero 
 			}
 		}
 	}
 
 	void OnCollisionEnter (Collision collision)
 	{
-		if (collision.gameObject.tag == "Ground")
+		//TAGS TAGS TAGS 
+		if (collision.gameObject.tag == "Ground") //tag ground shit
 		{
-			canJump = true;
-			jumpTimer = .5f;
+			canJump = true; //if player is on the ground then they can jump
+			
+			jumpTimer = .3f; //timer resets
 		}
 
 		if (collision.gameObject.tag == "Water")
 		{
-			Destroy(gameObject);
+			SceneManager.LoadScene("Bridge");
 		}
 
 		if (collision.gameObject.tag == "Rock")
--- Assets/Scripts/rockStacking.cs
+++ Assets/Scripts/rockStacking.cs
@@ -6,11 +6,22 @@ public class rockStacking : MonoBehaviour
 {
 
 	public GameObject player;
-	public GameObject rock;
+	public GameObject rock1;
+	public GameObject rock2;
+	public GameObject rock3;
+
+	public bool rock1Loaded;
+	public bool rock2Loaded;
+	public bool rock3Loaded;
+
 	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		rock1Loaded = false;
+		rock2Loaded = false;
+		rock3Loaded = false;
+
 	}
 	
 	// Update is called once per frame
@@ -22,7 +33,34 @@ public class rockStacking : MonoBehaviour
 	{
 		if (player.GetComponent<movement>().rockCounter == 1)
 		{
-			Instantiate(rock);
+			if (rock1Loaded == false)
+			{
+				Instantiate(rock1, new Vector3(5.8f, 66.5f, 16.03982f), Quaternion.Euler(0f, 0f, 0f));
+				rock1Loaded = true;
+			}
+		}
+		if (player.GetComponent<movement>().rockCounter == 2)
+		{
+			if (rock1Loaded == false && rock2Loaded == false)
+			{
+				Instantiate(rock1, new Vector3(5.8f, 67.5f, 16.03982f), Quaternion.Euler(0f, 0f, 0f));
+				Instantiate(rock2, new Vector3(5.8f, 68.5f, 16.03982f), Quaternion.Euler(0f, 0f, 0f));
+				rock1Loaded = true;
+				rock2Loaded = true;
+			}
+		}
+		if (player.GetComponent<movement>().rockCounter == 3)
+		{
+			if (rock1Loaded == false && rock2Loaded == false && rock3Loaded == false)
+			{
+				Instantiate(rock1, new Vector3(5.8f, 67.5f, 16.03982f), Quaternion.Euler(0f, 0f, 0f));
+				Instantiate(rock2, new Vector3(5.8f, 68.5f, 16.03982f), Quaternion.Euler(0f, 0f, 0f));
+				Instantiate(rock3, new Vector3(5.8f, 70.5f, 16.03982f), Quaternion.Euler(0f, 0f, 0f));
+				rock1Loaded = true;
+				rock2Loaded = true;
+				rock3Loaded = true;
+			}
+
 		}
 	}
 }
